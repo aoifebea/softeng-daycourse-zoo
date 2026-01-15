12af6331a4d2faf66cbd32a9a03315f58bf2b30a
aoifebea <aoifebea@gmail.com>
2026-01-15 14:37:03 +0000

    adding a tiger test

--- src/zoo/tiger.py
+++ src/zoo/tiger.py
@@ -4,12 +4,15 @@ from .animal import Animal
 
 
 class Tiger(Animal):
-    def __init__(self, name="tiger"):
+    def __init__(self, name="Tiger"):
         super().__init__(name, species="Cat")
 
     def sound(self):
-        return "meows and raws"
+        return "meow"
 
     def action(self):
-        return "prowls around, pounces to attack."
+        return "prowls around."
 
+# Testing
+def test_tiger():
+    assert Tiger().describe() == "Tiger the Cat says meow and prowls around."
